60b0ed45596d14b90c852b53690b8c788ef4efb5
syamaguc <programmer2844@gmail.com>
2023-11-10 09:29:43 +0900

    chore

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import blog, { ga } from "https://deno.land/x/blog/blog.tsx";
 // prismjs
-import "https://esm.sh/prismjs@1.27.0/components/prism-typescript?no-check";
-import "https://esm.sh/prismjs@1.27.0/components/prism-python?no-check";
+import "https://esm.sh/prismjs/components/prism-typescript?no-check";
+import "https://esm.sh/prismjs/components/prism-python?no-check";
 
 blog({
   author: "syamaguc",
@@ -9,11 +9,14 @@ blog({
   description: "The appearance of a child and the intellect of a child.",
   avatar: "haibara_ai_square.png",
   avatarClass: "rounded-full",
+  //theme: "light",
+  //cover: "#F8F8F2",
+  //coverTextColor: "#BD93F9",
   links: [
     { title: "GitHub", url: "https://github.com/syamaguc" },
     { title: "Twitter", url: "https://twitter.com/syamaguc" },
   ],
-  lang: "ja",
+  lang: "jp",
   dateFormat: (date) =>
     new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
   middlewares: [ga("G-TV5MK3SE7E")],
